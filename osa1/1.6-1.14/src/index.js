875e213c1fb7dddbe80683c23314ba68efd6a9c6
MJL7068 <mattil@netti.fi>
2019-01-16 01:04:31 +0200

    1.7 done

--- osa1/1.6-1.14/src/index.js
+++ osa1/1.6-1.14/src/index.js
@@ -1,52 +1,6 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-/*const Hello = ({name, age}) => {
-    const bornYear = () => new Date().getFullYear() - age
-
-    return (
-        <div>
-            <p>Hello {name}, you are {age} years old</p>
-            <p>So you were probably born {bornYear()}</p>
-        </div>
-    )
-}*/
-
-/*
-const App = () => {
-    const nimi = 'Pekka'
-    const ika = 10
-
-    return (
-      <div>
-        <h1>Greetings</h1>
-        <Hello name="Arto" age={26 + 10} />
-        <Hello name={nimi} age={ika}/>
-      </div>
-    )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
-*/
-
-//const Display = ({counter}) => <div>{counter}</div>
-
-const History = (props) => {
-    if (props.allClicks.length === 0) {
-        return (
-            <div>
-                sovellusta käytetään nappeja painelemalla
-            </div>
-        )
-    }
-
-    return (
-        <div>
-            näppäilyhistoria: {props.allClicks.join(' ')}
-        </div>
-    )
-}
-
 const Header = (props) => {
     return (
         <div>
@@ -73,31 +27,23 @@ const Feedback = (props) => {
 }
 
 const Statistics = (props) => {
+    const count = props.good + props.neutral + props.bad
+    const avg = (props.good * 1 + props.bad * -1)/3.0
+    const positives = 100*(props.good/count)
     return (
         <div>
           <Header text='statistiikka'/>
           <p>hyvä {props.good}</p>
           <p>neutraali {props.neutral}</p>
           <p>huono {props.bad}</p>
+          <p>yhteensä {count}</p>
+          <p>keskiarvo {avg}</p>
+          <p>positiivisia {positives} %</p>
         </div>
     )
 }
 
 const App = (props) => {
-    /*const [left, setLeft] = useState(0)
-    const [right, setRight] = useState(0)
-    const [allClicks, setAll] = useState([])
-
-    const handleLeftClick = () => {
-        setAll(allClicks.concat('L'))
-        setLeft(left + 1)
-    }
-
-    const handleRightClick = () => {
-        setAll(allClicks.concat('R'))
-        setRight(left + 1)
-    }*/
-
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
@@ -120,18 +66,6 @@ const App = (props) => {
             <Statistics good={good} neutral={neutral} bad={bad}/>
         </div>
     )
-
-    /*return (
-        <div>
-            <div>
-                {left}
-                <Button handleClick={handleLeftClick} text='vasen'/>
-                <Button handleClick={handleRightClick} text='oikea'/>
-              handleGood  {right}
-                <History allClicks={allClicks} />
-            </div>
-        </div>
-    )*/
 }
 
 ReactDOM.render(
