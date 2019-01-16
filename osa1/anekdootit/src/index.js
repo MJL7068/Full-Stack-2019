81f7b3717d95697529334fa4487fc1b44988da34
MJL7068 <mattil@netti.fi>
2019-01-16 20:33:33 +0200

    1.14 done

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -7,27 +7,65 @@ const Button = ({handleClick, text}) => (
     </button>
 )
 
+const Header = (props) => {
+  return(
+    <h1>{props.text}</h1>
+  )
+}
+
+const Voting = (props) => {
+  return(
+  <div>
+    <Header text='Anecdote of the day' />
+    <p>{props.anecdotes[props.selected]}</p>
+    <p>has {props.votes} votes</p>
+    <Button handleClick={props.handleSelected} text='next anecdote'/>
+    <Button handleClick={props.handleVote} text='vote'/>
+  </div>  
+  )
+}
+
+const MostVotes = (props) => {
+  return (
+    <div>
+      <Header text='Anecdote with most votes' />
+      <p>{props.anecdotes[props.highest]}</p>
+      <p>has {props.points[props.highest]} votes</p>
+    </div>
+  )
+}
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(0)
 
   const handleSelected = () => {
     const number = Math.floor(Math.random() * 6)
     setSelected(number)
+    setVotes(props.points[number])
   }
 
   const handleVote = () => {
-    points[selected] += 1
-    console.log(points)
+    props.points[selected] += 1
+    setVotes(props.points[selected])
   }
 
-  return (
-    <div>
-      <p>{props.anecdotes[selected]}</p>
-      <p>has {points[selected]} votes</p>
-      <Button handleClick={handleSelected} text='next anecdote'/>
-      <Button handleClick={handleVote} text='vote'/>
-    </div>
-  )
+  const highest = points.indexOf(Math.max(...points))
+
+  if (Math.max(...points) > 0) {
+    return (
+      <div>
+      <Voting anecdotes={props.anecdotes} points={points} selected={selected} votes={votes} handleSelected={handleSelected} handleVote={handleVote}/>
+      <MostVotes anecdotes={props.anecdotes} highest={highest} points={props.points}/>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <Voting anecdotes={props.anecdotes} points={points} selected={selected} votes={votes} handleSelected={handleSelected} handleVote={handleVote}/>
+      </div>
+    )
+  }
 }
 
 const points = Array.apply(null, new Array(6)).map(Number.prototype.valueOf,0);
