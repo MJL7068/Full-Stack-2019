6e080ca6758e14d997dde64530ba2b0902b299c1
MJL7068 <mattil@netti.fi>
2019-01-17 11:43:00 +0200

    2.4 done

--- osa2/2/src/components/Course.js
+++ osa2/2/src/components/Course.js
@@ -2,9 +2,7 @@ import React from 'react'
 
 const Header = (props) => {
     return (
-        <>
           <h1>{props.name}</h1>
-        </>
     )
 }
 
--- osa2/2/src/index.js
+++ osa2/2/src/index.js
@@ -3,30 +3,57 @@ import ReactDOM from 'react-dom'
 import Course from './components/Course'
 
 const App = () => {
-    const course = {
-      name: 'Half Stack -sovelluskehitys',
-      parts: [
+    const courses = [
         {
-          name: 'Reactin perusteet',
-          exercises: 10,
-          id: 1
+          name: 'Half Stack -sovelluskehitys',
+          id: 1,
+          parts: [
+            {
+              name: 'Reactin perusteet',
+              exercises: 10,
+              id: 1
+            },
+            {
+              name: 'Tiedonvälitys propseilla',
+              exercises: 7,
+              id: 2
+            },
+            {
+              name: 'Komponenttien tila',
+              exercises: 14,
+              id: 3
+            }
+          ]
         },
         {
-          name: 'Tiedonvälitys propseilla',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'Komponenttien tila',
-          exercises: 14,
-          id: 3
+          name: 'Node.js',
+          id: 2,
+          parts: [
+            {
+              name: 'Routing',
+              exercises: 2,
+              id: 1
+            },
+            {
+              name: 'Middlewaret',
+              exercises: 7,
+              id: 2
+            }
+          ]
         }
       ]
-    }
+
+    const rows = () => courses.map(course =>
+        <Course
+          key={course.id}
+          course={course}
+        />
+    )
   
     return (
       <div>
-        <Course course={course} />
+        <h1>Opetusohjelma</h1>
+        {rows()}
       </div>
     )
   }
